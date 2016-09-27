88de6a7fcc5121706d710552001efee451a18b01
haocan <1573636504@qq.com>
2016-09-27 16:22:46 +0800

    zy

---
+++ data/userInfo.js
@@ -0,0 +1,36 @@
+
+/*个人信息说明，数据格式为对象
+	*   {
+	*       userName: @value String               *         姓名
+			userPortrait: @value String           *         头像
+	*       jobWant: @value String                *         求职意向
+	*       userQQ: @value String|Number          *         QQ号码
+	*       userEmail: @value String              *         邮箱地址
+	*       motto: @value String                  []         激励格言
+	*       userPhone: @value String|Number       *          手机号码
+	*       userOrignPlace: @value String         *         籍贯
+	*       userSeatPlace: @value String          *         所在地
+	*       userWeibo：@value String              []         微博地址
+	*       userAssessment: @value String         []        自我评价
+	*       userHobby: @value Array              []          爱好
+	*       userAward:@value Array               []          奖项
+	*
+	*
+	*   }
+*/
+
+var userInfo = {
+    userName: "郝灿" , 
+    userPortrait:"images/icon/intro-03.png",
+    jobWant:  "WEB前端开发工程师",
+    userQQ:  "1573636504",
+    userEmail:   "1573636504@qq.com",
+    motto:  "置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你", 
+    userPhone: 15512851217,
+    userOrignPlace:  "河北省邢台地区南宫市" ,
+    userSeatPlace:  "北京市",
+    userWeibo: "<a href='https://haocan.github.io/index' target='_blank'>1111111111111</a>",
+    userAssessment: "有互联网工作经验,熟练掌握html,css和js",   
+    userHobby: ["运动","游戏","电影"],
+}
+
