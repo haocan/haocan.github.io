c9b7b7db33f324ac7185c99e650b111625d65c9a
haocan <1573636504@qq.com>
2016-11-06 18:24:44 +0800

    jd

--- data/project.js
+++ data/project.js
@@ -14,28 +14,28 @@
 
 var projects = [
 		{
-			projectName:"页面点击切换、移入移出、音乐播放器",
+			projectName:"仿京东PC端",
 			projectWebsite:"../jdpc/index.html",
-		    startTime:"2015.09.01",
-			endTime:"至今",
-			projectExplain:"页面点击切换、移入移出、音乐播放器、自定义滚动条，移入列表动画，vedio移入时根据移入方向显示透明层，播放器切歌，一张一张替换图片",
-			projectLabel:["html","css3","css","js"],
-			projectThumbnail:"images/icon/1.png"
+		    startTime:"2015.10.20",
+			endTime:"",
+			projectExplain:"仿京东PC端静态页面",
+			projectLabel:["html","css3","css"],
+			projectThumbnail:"images/icon/ser_09.jpg"
 		},
 		{
-			projectName:"页面点击切换、移入移出、音乐播放器",
+			projectName:"仿京东app端",
 			projectWebsite:"../jdapp/index.html",
-		    startTime:"2015.09.01",
-			endTime:"至今",
-			projectExplain:"页面点击切换、移入移出、音乐播放器、自定义滚动条，移入列表动画，vedio移入时根据移入方向显示透明层，播放器切歌，一张一张替换图片",
-			projectLabel:["html","css3","css","js"],
-			projectThumbnail:"images/icon/1.png"
+		    startTime:"2015.11.01",
+			endTime:"",
+			projectExplain:"手机端的京东页面"
+			projectLabel:["html","css3","css"],
+			projectThumbnail:"images/icon/8.jpg"
 		},
 		{
 			projectName:"页面点击切换、移入移出、音乐播放器",
 			projectWebsite:"../work/index.html",
-		    startTime:"2015.09.01",
-			endTime:"至今",
+		    startTime:"2016.09.01",
+			endTime:"",
 			projectExplain:"页面点击切换、移入移出、音乐播放器、自定义滚动条，移入列表动画，vedio移入时根据移入方向显示透明层，播放器切歌，一张一张替换图片",
 			projectLabel:["html","css3","css","js"],
 			projectThumbnail:"images/icon/1.png"
@@ -43,8 +43,8 @@ var projects = [
 		{
 			projectName:"滑屏效果",
 			projectWebsite:"../work1/index.html",
-		    startTime:"2015.09.01",
-			endTime:"至今",
+		    startTime:"2016.10.01",
+			endTime:"",
 			projectExplain:"滚轮事件，分为4块，轮播图，淡入淡出，放大缩小",
 			projectLabel:["html","css3","css","js"],
 			projectThumbnail:"images/icon/project-2.jpg"
